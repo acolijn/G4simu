19f382e30b167fadaba09ce5f346ce5412388294
acolijn <auke.pieter.colijn@gmail.com>
2014-11-13 18:25:57 +0100

    bug fixes

--- include/AnalysisManager.hh
+++ include/AnalysisManager.hh
@@ -15,51 +15,54 @@ class G4Step;
 class TFile;
 class TTree;
 
+class AnalysisMessenger;
 class EventData;
 class PrimaryGeneratorAction;
 
 class AnalysisManager
 {
 public:
-  AnalysisManager(PrimaryGeneratorAction *pPrimaryGeneratorAction);
-  virtual ~AnalysisManager();
-
+    AnalysisManager(PrimaryGeneratorAction *pPrimaryGeneratorAction);
+    virtual ~AnalysisManager();
+    
 public:
-  virtual void BeginOfRun(const G4Run *pRun); 
-  virtual void EndOfRun(const G4Run *pRun); 
-  virtual void BeginOfEvent(const G4Event *pEvent); 
-  virtual void EndOfEvent(const G4Event *pEvent); 
-  virtual void Step(const G4Step *pStep);	
-  
-  void SetDataFilename(const G4String &hFilename) { m_hDataFilename = hFilename; }
-  void SetNbEventsToSimulate(G4int iNbEventsToSimulate) { m_iNbEventsToSimulate = iNbEventsToSimulate;}
-
+    virtual void BeginOfRun(const G4Run *pRun);
+    virtual void EndOfRun(const G4Run *pRun);
+    virtual void BeginOfEvent(const G4Event *pEvent);
+    virtual void EndOfEvent(const G4Event *pEvent);
+    virtual void Step(const G4Step *pStep);
+    
+    void SetDataFilename(const G4String &hFilename) { m_hDataFilename = hFilename; }
+    void SetNbEventsToSimulate(G4int iNbEventsToSimulate) { m_iNbEventsToSimulate = iNbEventsToSimulate;}
+    void SetTreeType(G4String ttype){m_hTreeType=ttype;}
+    
 private:
-  G4bool FilterEvent(EventData *pEventData);
-
+    G4bool FilterEvent(EventData *pEventData);
+    
 private:
-  G4int m_LXeCollectionID;
-  G4int m_NaICollectionID;
-  vector<G4int> m_CollectionIDs;
-
-  G4String m_hDataFilename;
-  G4int m_iNbEventsToSimulate;
-
-  TFile      *m_pTreeFile;
-  TTree      *m_pTree;
-  TDirectory *_events;
-
-  TParameter<int> *m_pNbEventsToSimulateParameter;
-  //  TParameter<double> *nRejectParameter;
-  // TParameter<double> *nAcceptParameter;
-
-  PrimaryGeneratorAction *m_pPrimaryGeneratorAction;
-
-  EventData *m_pEventData;
-  G4bool            plotPhysics;
-
-  G4Timer *runTime;
-  G4bool            writeEmptyEvents;
+    G4int m_LXeCollectionID;
+    G4int m_NaICollectionID;
+    vector<G4int> m_CollectionIDs;
+    
+    G4String m_hDataFilename;
+    G4String m_hTreeType;
+    G4int m_iNbEventsToSimulate;
+    
+    TFile      *m_pTreeFile;
+    TTree      *m_pTree;
+    TDirectory *_events;
+    
+    TParameter<int> *m_pNbEventsToSimulateParameter;
+    //  TParameter<double> *nRejectParameter;
+    // TParameter<double> *nAcceptParameter;
+    
+    PrimaryGeneratorAction *m_pPrimaryGeneratorAction;
+    
+    EventData *m_pEventData;
+    G4bool            plotPhysics;
+    
+    G4Timer *runTime;
+    G4bool            writeEmptyEvents;
 };
 
 #endif 
--- src/AnalysisManager.cc
+++ src/AnalysisManager.cc
@@ -9,7 +9,7 @@
 #include <G4NistManager.hh>
 #include <G4ElementTable.hh>
 #include <G4Version.hh>
-//#include <G4Element.hh> 
+//#include <G4Element.hh>
 #include <numeric>
 
 #include <TROOT.h>
@@ -29,18 +29,18 @@ using namespace CLHEP;
 
 AnalysisManager::AnalysisManager(PrimaryGeneratorAction *pPrimaryGeneratorAction)
 {
-  runTime = new G4Timer();
-  m_CollectionIDs.clear();
-  
-  m_LXeCollectionID = -1;
-  m_NaICollectionID = -1;
-  m_hDataFilename = "events.root";
-  
-  m_pPrimaryGeneratorAction = pPrimaryGeneratorAction;
-  
-  m_pEventData = new EventData();
-  //plotPhysics      = kTRUE;
-  writeEmptyEvents = kTRUE; 
+    runTime = new G4Timer();
+    m_CollectionIDs.clear();
+    
+    m_LXeCollectionID = -1;
+    m_NaICollectionID = -1;
+    m_hDataFilename = "events.root";
+    
+    m_pPrimaryGeneratorAction = pPrimaryGeneratorAction;
+    
+    m_pEventData = new EventData();
+    //plotPhysics      = kTRUE;
+    writeEmptyEvents = kTRUE;
 }
 
 AnalysisManager::~AnalysisManager()
@@ -50,173 +50,177 @@ AnalysisManager::~AnalysisManager()
 void
 AnalysisManager::BeginOfRun(const G4Run *)
 {
-  // start a timer for this run....
-  runTime->Start();
-  // do we write empty events or not?
-  writeEmptyEvents = kTRUE;
-  
-  m_pTreeFile = new TFile(m_hDataFilename.c_str(), "RECREATE");
-  // make tree structure
-  TNamed *G4version = new TNamed("G4VERSION_TAG",G4VERSION_TAG);
-  G4version->Write();
+    // start a timer for this run....
+    runTime->Start();
+    // do we write empty events or not?
+    writeEmptyEvents = kTRUE;
     
-  _events = m_pTreeFile->mkdir("events");
-  _events->cd();
-
-  G4cout <<"AnalysisManager:: Init data tree ..."<<G4endl;
-  m_pTree = new TTree("events", "Event Data");
-
-  gROOT->ProcessLine("#include <vector>");
-  
-  m_pTree->Branch("eventid", &m_pEventData->m_iEventId, "eventid/I");
-
-  m_pTree->Branch("etot", &m_pEventData->m_fTotalEnergyDeposited, "etot/F");
-  m_pTree->Branch("nsteps", &m_pEventData->m_iNbSteps, "nsteps/I");
-	
-  m_pTree->Branch("trackid", "vector<int>", &m_pEventData->m_pTrackId);
-  m_pTree->Branch("type", "vector<string>", &m_pEventData->m_pParticleType);
-  m_pTree->Branch("parentid", "vector<int>", &m_pEventData->m_pParentId);
-  m_pTree->Branch("collid", "vector<int>", &m_pEventData->m_pCollectionId);
-  m_pTree->Branch("parenttype", "vector<string>", &m_pEventData->m_pParentType);
-  m_pTree->Branch("creaproc", "vector<string>", &m_pEventData->m_pCreatorProcess);
-  m_pTree->Branch("edproc", "vector<string>", &m_pEventData->m_pDepositingProcess);
-  m_pTree->Branch("etot_det", "vector<float>", &m_pEventData->m_pEtot);
-  m_pTree->Branch("xp", "vector<float>", &m_pEventData->m_pX);
-  m_pTree->Branch("yp", "vector<float>", &m_pEventData->m_pY);
-  m_pTree->Branch("zp", "vector<float>", &m_pEventData->m_pZ);
-  m_pTree->Branch("ed", "vector<float>", &m_pEventData->m_pEnergyDeposited);
-  m_pTree->Branch("time", "vector<float>", &m_pEventData->m_pTime);
-
-  m_pTree->Branch("type_pri", "vector<string>", &m_pEventData->m_pPrimaryParticleType);
-  m_pTree->Branch("xp_pri", &m_pEventData->m_fPrimaryX, "xp_pri/F");
-  m_pTree->Branch("yp_pri", &m_pEventData->m_fPrimaryY, "yp_pri/F");
-  m_pTree->Branch("zp_pri", &m_pEventData->m_fPrimaryZ, "zp_pri/F");
-  m_pTree->Branch("e_pri",  &m_pEventData->m_fPrimaryE, "e_pri/F");
-  m_pTree->Branch("w_pri",  &m_pEventData->m_fPrimaryW, "w_pri/F");
-
-  m_pNbEventsToSimulateParameter = new TParameter<int>("nbevents", m_iNbEventsToSimulate);
-  m_pNbEventsToSimulateParameter->Write();
-
-  m_pTreeFile->cd();
-
+    m_pTreeFile = new TFile(m_hDataFilename.c_str(), "RECREATE");
+    // make tree structure
+    TNamed *G4version = new TNamed("G4VERSION_TAG",G4VERSION_TAG);
+    G4version->Write();
+    
+    _events = m_pTreeFile->mkdir("events");
+    _events->cd();
+    
+    G4cout <<"AnalysisManager:: Init data tree ..."<<G4endl;
+    m_pTree = new TTree("events", "Event Data");
+    
+    gROOT->ProcessLine("#include <vector>");
+    
+    m_pTree->Branch("eventid", &m_pEventData->m_iEventId, "eventid/I");
+    
+    m_pTree->Branch("etot", &m_pEventData->m_fTotalEnergyDeposited, "etot/F");
+    m_pTree->Branch("nsteps", &m_pEventData->m_iNbSteps, "nsteps/I");
+    
+    m_pTree->Branch("trackid", "vector<int>", &m_pEventData->m_pTrackId);
+    m_pTree->Branch("type", "vector<string>", &m_pEventData->m_pParticleType);
+    m_pTree->Branch("parentid", "vector<int>", &m_pEventData->m_pParentId);
+    m_pTree->Branch("collid", "vector<int>", &m_pEventData->m_pCollectionId);
+    m_pTree->Branch("parenttype", "vector<string>", &m_pEventData->m_pParentType);
+    m_pTree->Branch("creaproc", "vector<string>", &m_pEventData->m_pCreatorProcess);
+    m_pTree->Branch("edproc", "vector<string>", &m_pEventData->m_pDepositingProcess);
+    m_pTree->Branch("etot_det", "vector<float>", &m_pEventData->m_pEtot);
+    m_pTree->Branch("xp", "vector<float>", &m_pEventData->m_pX);
+    m_pTree->Branch("yp", "vector<float>", &m_pEventData->m_pY);
+    m_pTree->Branch("zp", "vector<float>", &m_pEventData->m_pZ);
+    m_pTree->Branch("ed", "vector<float>", &m_pEventData->m_pEnergyDeposited);
+    m_pTree->Branch("time", "vector<float>", &m_pEventData->m_pTime);
+    
+    m_pTree->Branch("type_pri", "vector<string>", &m_pEventData->m_pPrimaryParticleType);
+    m_pTree->Branch("xp_pri", &m_pEventData->m_fPrimaryX, "xp_pri/F");
+    m_pTree->Branch("yp_pri", &m_pEventData->m_fPrimaryY, "yp_pri/F");
+    m_pTree->Branch("zp_pri", &m_pEventData->m_fPrimaryZ, "zp_pri/F");
+    m_pTree->Branch("e_pri",  &m_pEventData->m_fPrimaryE, "e_pri/F");
+    m_pTree->Branch("w_pri",  &m_pEventData->m_fPrimaryW, "w_pri/F");
+    
+    m_pNbEventsToSimulateParameter = new TParameter<int>("nbevents", m_iNbEventsToSimulate);
+    m_pNbEventsToSimulateParameter->Write();
+    
+    m_pTreeFile->cd();
+    
 }
 
 void
 AnalysisManager::EndOfRun(const G4Run *)
 {
-  runTime->Stop();
-  G4double dt = runTime->GetRealElapsed();
-  // make tree structure
-  TParameter<G4double> *dtPar = new TParameter<G4double>("G4RUNTIME",dt);
-  dtPar->Write();
-
-  m_pTreeFile->cd();
-
-  m_pTreeFile->Write();
-  m_pTreeFile->Close();
+    runTime->Stop();
+    G4double dt = runTime->GetRealElapsed();
+    // make tree structure
+    TParameter<G4double> *dtPar = new TParameter<G4double>("G4RUNTIME",dt);
+    dtPar->Write();
+    
+    m_pTreeFile->cd();
+    
+    m_pTreeFile->Write();
+    m_pTreeFile->Close();
 }
 
 void
 AnalysisManager::BeginOfEvent(const G4Event *)
 {
-  if(m_LXeCollectionID == -1)
+    if(m_LXeCollectionID == -1)
     {
-      G4SDManager *pSDManager = G4SDManager::GetSDMpointer();
-      m_LXeCollectionID = pSDManager->GetCollectionID("LXe/HitsCollection");
-      m_CollectionIDs.push_back(m_LXeCollectionID);
-      m_NaICollectionID = pSDManager->GetCollectionID("NaI/HitsCollection");
-      m_CollectionIDs.push_back(m_NaICollectionID);
-      G4cout << "AnalysisManager::BeginOfEvent  Found LXe collection at ID = " << m_LXeCollectionID <<G4endl;
-      G4cout << "AnalysisManager::BeginOfEvent  Found NaI collection at ID = " << m_NaICollectionID <<G4endl;
-    } 
-
+        G4SDManager *pSDManager = G4SDManager::GetSDMpointer();
+        m_LXeCollectionID = pSDManager->GetCollectionID("LXe/HitsCollection");
+        m_CollectionIDs.push_back(m_LXeCollectionID);
+        m_NaICollectionID = pSDManager->GetCollectionID("NaI/HitsCollection");
+        m_CollectionIDs.push_back(m_NaICollectionID);
+        G4cout << "AnalysisManager::BeginOfEvent  Found LXe collection at ID = " << m_LXeCollectionID <<G4endl;
+        G4cout << "AnalysisManager::BeginOfEvent  Found NaI collection at ID = " << m_NaICollectionID <<G4endl;
+    }
+    
 }
 
 void
 AnalysisManager::EndOfEvent(const G4Event *pEvent)
 {
-  _events->cd();
-
-  G4HCofThisEvent* pHCofThisEvent = pEvent->GetHCofThisEvent();
-
-  // get the event ID and primary particle information
-  m_pEventData->m_iEventId = pEvent->GetEventID();
-  m_pEventData->m_pPrimaryParticleType->push_back(m_pPrimaryGeneratorAction->GetParticleTypeOfPrimary());
-  
-  m_pEventData->m_fPrimaryX = m_pPrimaryGeneratorAction->GetPositionOfPrimary().x();
-  m_pEventData->m_fPrimaryY = m_pPrimaryGeneratorAction->GetPositionOfPrimary().y();
-  m_pEventData->m_fPrimaryZ = m_pPrimaryGeneratorAction->GetPositionOfPrimary().z();
-  m_pEventData->m_fPrimaryE = m_pPrimaryGeneratorAction->GetEnergyOfPrimary() / keV;
-  m_pEventData->m_fPrimaryW = pEvent->GetPrimaryVertex()->GetWeight();
- 
-  
-  // uppack teh hit collections
-  G4int    iNbHits = 0;
-  G4int    iNbSteps = 0;
-  G4double fEtot = 0;
-	
-  if(pHCofThisEvent) {
-    // loop over all our hit collections
-    for(G4int icol=0; icol<m_CollectionIDs.size(); icol++){
-      if(m_CollectionIDs[icol] != -1){
-        stdHitsCollection* pHitsCollection = 0;
-        pHitsCollection = static_cast<stdHitsCollection*>(pHCofThisEvent->GetHC(m_CollectionIDs[icol]));
-        iNbHits = (pHitsCollection)?(pHitsCollection->entries()):(0);
-        G4cout << icol << " Nb hits = " << iNbHits<< " ID = "<<m_CollectionIDs[icol]<<G4endl;
-
-
-        G4float fTotalEnergyDeposited = 0.;
-        if(iNbHits) {
-          // hits
-          for(G4int i=0; i<iNbHits; i++) {
-            stdHit *pHit = (*pHitsCollection)[i];
-
-            if(pHit->GetParticleType() != "opticalphoton"){
-       	      m_pEventData->m_pTrackId->push_back(pHit->GetTrackId());
-	      m_pEventData->m_pParentId->push_back(pHit->GetParentId());
-	      m_pEventData->m_pCollectionId->push_back(icol);
-
-	      m_pEventData->m_pParticleType->push_back(pHit->GetParticleType());
-	      m_pEventData->m_pParentType->push_back(pHit->GetParentType());
-	      m_pEventData->m_pCreatorProcess->push_back(pHit->GetCreatorProcess());
-	      m_pEventData->m_pDepositingProcess->push_back(pHit->GetDepositingProcess());
-
-	      m_pEventData->m_pX->push_back(pHit->GetPosition().x()/mm);
-	      m_pEventData->m_pY->push_back(pHit->GetPosition().y()/mm);
-	      m_pEventData->m_pZ->push_back(pHit->GetPosition().z()/mm);
-
-	      fTotalEnergyDeposited += pHit->GetEnergyDeposited()/keV;
-	      m_pEventData->m_pEnergyDeposited->push_back(pHit->GetEnergyDeposited()/keV);
-
-	      m_pEventData->m_pKineticEnergy->push_back(pHit->GetKineticEnergy()/keV);
-	      m_pEventData->m_pTime->push_back(pHit->GetTime()/second);
-
-	      iNbSteps++;
-	    } // if !opticalphoton
-          } // loop over hits 
-	} // if Hits
-        m_pEventData->m_pEtot->push_back(fTotalEnergyDeposited);
-        fEtot += fTotalEnergyDeposited;
-        
-      }
+    _events->cd();
+    
+    G4HCofThisEvent* pHCofThisEvent = pEvent->GetHCofThisEvent();
+    
+    // get the event ID and primary particle information
+    m_pEventData->m_iEventId = pEvent->GetEventID();
+    m_pEventData->m_pPrimaryParticleType->push_back(m_pPrimaryGeneratorAction->GetParticleTypeOfPrimary());
+    
+    m_pEventData->m_fPrimaryX = m_pPrimaryGeneratorAction->GetPositionOfPrimary().x();
+    m_pEventData->m_fPrimaryY = m_pPrimaryGeneratorAction->GetPositionOfPrimary().y();
+    m_pEventData->m_fPrimaryZ = m_pPrimaryGeneratorAction->GetPositionOfPrimary().z();
+    m_pEventData->m_fPrimaryE = m_pPrimaryGeneratorAction->GetEnergyOfPrimary() / keV;
+    m_pEventData->m_fPrimaryW = pEvent->GetPrimaryVertex()->GetWeight();
+    
+    
+    // uppack teh hit collections
+    G4int    iNbHits = 0;
+    G4int    iNbSteps = 0;
+    G4double fEtot = 0;
+    
+    if(pHCofThisEvent) {
+        //
+        // loop over all our hit collections
+        //
+        for(G4int icol=0; icol<m_CollectionIDs.size(); icol++){
+            // check if the ID of the collection is OK
+            if(m_CollectionIDs[icol] != -1){
+                stdHitsCollection* pHitsCollection = 0;
+                // get the hits
+                pHitsCollection = static_cast<stdHitsCollection*>(pHCofThisEvent->GetHC(m_CollectionIDs[icol]));
+                // the number of hits
+                iNbHits = (pHitsCollection)?(pHitsCollection->entries()):(0);
+                G4cout << icol << " Nb hits = " << iNbHits<< " ID = "<<m_CollectionIDs[icol]<<G4endl;
+                
+                G4float fTotalEnergyDeposited = 0.;
+                if(iNbHits) {
+                    // hits
+                    for(G4int i=0; i<iNbHits; i++) {
+                        stdHit *pHit = (*pHitsCollection)[i];
+                        
+                        if(pHit->GetParticleType() != "opticalphoton"){
+                            m_pEventData->m_pTrackId->push_back(pHit->GetTrackId());
+                            m_pEventData->m_pParentId->push_back(pHit->GetParentId());
+                            m_pEventData->m_pCollectionId->push_back(icol);
+                            
+                            m_pEventData->m_pParticleType->push_back(pHit->GetParticleType());
+                            m_pEventData->m_pParentType->push_back(pHit->GetParentType());
+                            m_pEventData->m_pCreatorProcess->push_back(pHit->GetCreatorProcess());
+                            m_pEventData->m_pDepositingProcess->push_back(pHit->GetDepositingProcess());
+                            
+                            m_pEventData->m_pX->push_back(pHit->GetPosition().x()/mm);
+                            m_pEventData->m_pY->push_back(pHit->GetPosition().y()/mm);
+                            m_pEventData->m_pZ->push_back(pHit->GetPosition().z()/mm);
+                            
+                            fTotalEnergyDeposited += pHit->GetEnergyDeposited()/keV;
+                            m_pEventData->m_pEnergyDeposited->push_back(pHit->GetEnergyDeposited()/keV);
+                            
+                            m_pEventData->m_pKineticEnergy->push_back(pHit->GetKineticEnergy()/keV);
+                            m_pEventData->m_pTime->push_back(pHit->GetTime()/second);
+                            
+                            iNbSteps++;
+                        } // if !opticalphoton
+                    } // loop over hits 
+                } // if Hits
+                m_pEventData->m_pEtot->push_back(fTotalEnergyDeposited);
+                fEtot += fTotalEnergyDeposited;
+                
+            }
+        }
     }
-  }
-  
-  // also write the header information + primary vertex of the empty events....
-  m_pEventData->m_iNbSteps = iNbSteps;
-  m_pEventData->m_fTotalEnergyDeposited = fEtot;
-  
-  // save only energy depositing events
-  m_pTree->Fill(); // write all events to the tree
-  
-  m_pEventData->Clear(); 
-  m_pTreeFile->cd();
-
-//  delete pHitsCollection;
+    
+    // also write the header information + primary vertex of the empty events....
+    m_pEventData->m_iNbSteps = iNbSteps;
+    m_pEventData->m_fTotalEnergyDeposited = fEtot;
+    
+    // save only energy depositing events
+    m_pTree->Fill(); // write all events to the tree
+    
+    m_pEventData->Clear(); 
+    m_pTreeFile->cd();
+    
+    //  delete pHitsCollection;
 }
 
 void
 AnalysisManager::Step(const G4Step *)
 {
-
+    
 }
